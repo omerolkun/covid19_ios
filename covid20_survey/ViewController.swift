93465eab8f0a98d9236d25b04d414546adb031dc
omercik <omerolkun1908@gmail.com>
2021-11-29 09:15:29 +0300

    Initial Commit

---
+++ covid20_survey/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  covid20_survey
+//
+//  Created by macbook on 29.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
